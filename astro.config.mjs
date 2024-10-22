fb3bfbe3069ff1dda6cf035d20bdcd99472208ad
Maciek Sitkowski <msitkowski94@gmail.com>
2024-10-22 21:30:23 +0200

    define path aliases in vite config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
-
-import tailwind from '@astrojs/tailwind';
+import {defineConfig} from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import path from 'path'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
-});
+  integrations: [react(), tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        '@assets': path.resolve('./src/assets'),
+        '@components': path.resolve('./src/components'),
+        '@layouts': path.resolve('./src/layouts'),
+        '@examples': path.resolve('./src/examples'),
+      },
+    },
+  },
+})
