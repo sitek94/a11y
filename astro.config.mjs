9e6901c9c4b287746266ed1c4c17e58bccdfa7ab
Maciek Sitkowski <msitkowski94@gmail.com>
2024-10-22 21:39:28 +0200

    temporarily get rid off path aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,8 @@
 import {defineConfig} from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
-import path from 'path'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  vite: {
-    resolve: {
-      alias: {
-        '@assets': path.resolve('src/assets'),
-        '@components': path.resolve('src/components'),
-        '@layouts': path.resolve('src/layouts'),
-        '@examples': path.resolve('src/examples'),
-      },
-    },
-  },
 })
